846520207d58f2aa92a4e535fc115fb528e5b6fd
DjinaStevkovska <stevkovskadjina@yahoo.com>
2020-05-20 10:43:05 +0200

    DjinaStevkovska

---
+++ Homework26_DjinaStevkovska/visit_counter/index.php
@@ -0,0 +1,28 @@
+<?php
+//start session
+session_start();
+
+// if session variable 'counter' not exist create it and set to 1, if it exist increase it by 1
+if (isset($_SESSION["counter"])){
+    $_SESSION["counter"] = $_SESSION["counter"] + 1;
+    $counter = $_SESSION["counter"];
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Visit counter</title>
+</head>
+<body>
+    <h1>Visit counter</h1>
+    <p>You opened this page: </p>
+    <?php 
+    if(isset($_SESSION["counter"])) {
+        echo "<h3>".$counter." times.</h3>";
+    }  
+    ?>
+</body>
+</html>
